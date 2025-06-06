81c8186721558da9cec5d33f2a42a1cba196bee3
PAPA <PAPA@kinal.edu.gt>
2025-06-06 07:53:44 -0600

    Se agrega el archivo app.js en configs y se modifica index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
     import { connect } from "./configs/mongo.js";
-    
+    import { initServer} from "./configs/app.js";
+    import { config } from "dotenv";
 
+    config()
+    initServer()
     connect()
