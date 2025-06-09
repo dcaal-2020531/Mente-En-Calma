ed13c1baad182b77600dca2aa54705c10daf3ed6
dcaal-2020531 <dcaal-2020531@kinal.edu.gt>
2025-06-09 11:53:11 -0600

    Se eliminan todos los archivos de la rama main

--- index.js
+++
@@ -1,8 +0,0 @@
-
-    import { connect } from "./configs/mongo.js";
-    import { initServer} from "./configs/app.js";
-    import { config } from "dotenv";
-
-    config()
-    initServer()
-    connect()
